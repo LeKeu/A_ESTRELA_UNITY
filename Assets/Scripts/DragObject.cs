d02a88f7f8e0fe002bd4ec0d04c0d651c7f0e500
LeKeu <lekeuffer@gmail.com>
2023-09-19 17:44:07 -0300

    drag e create obstcl

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    Plane movePlane;
+    float fixedDistance = 2f;
+    float hitDist, t;
+    Ray camRay;
+    Vector3 startPos, point, corPoint;
+
+    void OnMouseDown()
+    {
+        startPos = transform.position; // save position in case draged to invalid place
+        movePlane = new Plane(-Camera.main.transform.forward, transform.position); // find a parallel plane to the camera based on obj start pos;
+    }
+
+    void OnMouseDrag()
+    {
+        camRay = Camera.main.ScreenPointToRay(Input.mousePosition); // shoot a ray at the obj from mouse screen point
+
+        if (movePlane.Raycast(camRay, out hitDist))
+        { // finde the collision on movePlane
+            point = camRay.GetPoint(hitDist); // define the point on movePlane
+            t = -(fixedDistance - camRay.origin.y) / (camRay.origin.y - point.y); // the x,y or z plane you want to be fixed to
+            corPoint.x = camRay.origin.x + (point.x - camRay.origin.x) * t; // calculate the new point t futher along the ray
+            corPoint.y = 0;
+            corPoint.z = camRay.origin.z + (point.z - camRay.origin.z) * t;
+            transform.position = corPoint;
+        }
+    }
+}
